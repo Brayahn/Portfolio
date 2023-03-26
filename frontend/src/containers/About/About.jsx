912cd90ba58efe5f4f499979a3a2f4435dc4609c
Brayahn <brayahnmainah@gmail.com>
2023-03-26 21:33:06 +0300

    folder structure lineup

---
+++ frontend/src/containers/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './About.scss';
+
+const about = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react";
-import App from ".App";
+import ReactDOM from "react-dom";
+
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
