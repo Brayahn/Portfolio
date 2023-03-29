336207390c37a5f326e1b32db7eee109d1a1cda4
Brayahn <brayahnmainah@gmail.com>
2023-03-29 21:23:15 +0300

    template images

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Navbar.scss';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
+  return (<div>
+    <img src={images.logo} alt="logo"/></div>
   )
 }
 
