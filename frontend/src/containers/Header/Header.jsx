f9c9fc2d8abfe6ea001efae198b254321451b73a
Brayahn <brayahnmainah@gmail.com>
2023-04-10 19:16:55 +0300

    header styled

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -17,7 +17,7 @@ const scaleVariants ={
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div id="home" className='app__header app__flex'>
 <motion.div
 whileInView={{x: [-100, 0 ], opacity:[0,1] }}
 transition={{duration: 5}}
@@ -62,7 +62,7 @@ variant={scaleVariants}
 whileInView={scaleVariants.whileInView}
 className='app__header-circles'
 >
-{[images.flutter, images.redux, images.sass].map((circle, index)=>(
+{[images.git, images.react, images.sass].map((circle, index)=>(
   <div className='circle-cmp app__flex' key={`circle-${index}`}>
     <img src={circle} alt="circle"/>
   </div>
