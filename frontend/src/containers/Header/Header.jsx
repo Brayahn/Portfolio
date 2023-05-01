4000886ed6f43050f8dfeb51e978e44bdb9dd199
Brayahn <brayahnmainah@gmail.com>
2023-05-01 17:30:18 +0300

    HOC implemented and App Wrapper

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {delay, motion} from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
+import { AppWrap } from '../../wrapper';
 
 
 const scaleVariants ={
@@ -17,7 +18,7 @@ const scaleVariants ={
 
 const Header = () => {
   return (
-    <div id="home" className='app__header app__flex'>
+    <div className='app__header app__flex'>
 <motion.div
 whileInView={{x: [-100, 0 ], opacity:[0,1] }}
 transition={{duration: 5}}
@@ -73,4 +74,4 @@ className='app__header-circles'
   )
 }
 
-export default Header
+export default AppWrap(Header,'home');
