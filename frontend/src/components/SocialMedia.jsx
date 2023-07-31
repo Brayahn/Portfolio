a91e3b525ef45ffbadedeba522adfb8257fe722f
Brayahn <brayahnmainah@gmail.com>
2023-07-31 05:13:59 +0300

    social media and work cleanup

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import {BsTwitter, BsInstagram} from 'react-icons/bs';
+import {BsTwitter, BsInstagram, BsYoutube} from 'react-icons/bs';
 import {FaLinkedin} from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        
+         <div>
+            <FaLinkedin/>
+        </div>
         <div>
             <BsTwitter/>
         </div>
         <div>
-            <FaLinkedin/>
-        </div>
-        <div>
             <BsInstagram/>
+        </div> 
+        <div>
+            <BsYoutube/>
         </div> 
     </div>
   )
